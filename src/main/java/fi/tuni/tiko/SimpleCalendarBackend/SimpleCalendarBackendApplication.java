2700e671eb9c72ad62e402f38a446aea207ebeb1
Toni Vänttinen <toni.vanttinen@tuni.fi>
2019-03-31 20:08:25 +0300

    Initial Spring boot commit

---
+++ src/main/java/fi/tuni/tiko/SimpleCalendarBackend/SimpleCalendarBackendApplication.java
@@ -0,0 +1,13 @@
+package fi.tuni.tiko.SimpleCalendarBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCalendarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCalendarBackendApplication.class, args);
+    }
+
+}
