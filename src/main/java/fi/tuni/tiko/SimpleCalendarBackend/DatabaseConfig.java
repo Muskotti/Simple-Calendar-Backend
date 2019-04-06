a5fced9663d2d0de57d9e0058b2cbf696a4863a9
Toni Vänttinen <toni.vanttinen@tuni.fi>
2019-04-06 14:00:45 +0300

    Database test

--- src/main/java/fi/tuni/tiko/SimpleCalendarBackend/DatabaseConfig.java
+++ src/main/java/fi/tuni/tiko/SimpleCalendarBackend/DatabaseConfig.java
@@ -1,23 +1,30 @@
 package fi.tuni.tiko.SimpleCalendarBackend;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
+@ComponentScan(basePackages = "fi.tuni.tiko")
 public class DatabaseConfig {
 
-    @Value("${prmjxxufhbuacr:9fa1be1fdb4c5dd272119e280674fa79026e8f8e082daf1aeba263806611ba9c@ec2-23-23-241-119.compute-1.amazonaws.com:5432/d85unttfeo5dag}")
-    private String dbUrl;
-
     @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dbUrl);
-        return new HikariDataSource(config);
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
     }
 }
