19c9f37f02531244aa5574540b44cad33917bc85
Toni Vänttinen <toni.vanttinen@tuni.fi>
2019-04-21 19:26:41 +0300

    Test 4

--- src/main/java/fi/tuni/tiko/SimpleCalendarBackend/SimpleCalendarBackendApplication.java
+++ src/main/java/fi/tuni/tiko/SimpleCalendarBackend/SimpleCalendarBackendApplication.java
@@ -1,33 +1,13 @@
 package fi.tuni.tiko.SimpleCalendarBackend;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.transaction.Transactional;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-
 @SpringBootApplication
-public class SimpleCalendarBackendApplication implements CommandLineRunner {
-
-    @Autowired
-    EventRepository events;
+public class SimpleCalendarBackendApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleCalendarBackendApplication.class, args);
-    }
-
-    @Override
-    @Transactional
-    public void run(String... args) throws Exception {
-
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Calendar cal = Calendar.getInstance();
-        String newDate = dateFormat.format(cal.getTime());
-        SimpleEvent event1 = new SimpleEvent("Event1", "Event1 text",newDate);
-        events.save(event1);
     }
 }
